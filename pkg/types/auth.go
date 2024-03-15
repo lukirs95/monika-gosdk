e66ccdae5b7a1d8c9a3d64cd03c68a40ee5b3c07
Lucas Kirsche <kontakt@lucas-kirsche.de>
2024-03-15 14:21:10 +0100

    add auth object

--- pkg/types/auth.go
+++ pkg/types/auth.go
@@ -1,15 +1,15 @@
 package types
 
 type Auth struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Expires  bool   `json:"expires"`
+	Username Username `json:"username"`
+	Password Password `json:"password"`
+	Expires  bool     `json:"expires"`
 }
 
 func NewAuth(username string, password string, expires bool) *Auth {
 	return &Auth{
-		Username: username,
-		Password: password,
+		Username: Username(username),
+		Password: Password(password),
 		Expires:  expires,
 	}
 }
