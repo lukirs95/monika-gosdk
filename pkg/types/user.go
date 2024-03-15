acc48f467b7a152a18b7cadeebb4220679a1eee2
Lucas Kirsche <kontakt@lucas-kirsche.de>
2024-03-15 12:54:57 +0100

    improve user validations

--- pkg/types/user.go
+++ pkg/types/user.go
@@ -6,21 +6,32 @@ import (
 )
 
 type User struct {
-	Username string   `json:"username"`
-	Password string   `json:"password"`
+	Username Username `json:"username"`
+	Password Password `json:"password"`
 	Role     UserRole `json:"role"`
 }
 
 type Username string
 
+// Sanitizes username. Removes leading and trailing whitespace and
+// puts characters to lower case.
 func (username *Username) Sanitize() {
 	*username = Username(strings.ToLower(string(*username)))
 	*username = Username(strings.TrimSpace(string(*username)))
 }
 
-func (username Username) Validate() bool {
+// Validates username. Contstraints: 4-30 chars, chars: {a-z, A-Z, 0-9, `@`, `.`, `_`, `-`}
+func (username Username) Valid() bool {
 	validUser := regexp.MustCompile(`^[a-zA-Z0-9@\._-]{4,30}$`)
 	return validUser.MatchString(string(username))
+}
+
+type Password string
+
+// Validates Password. Constraints: 10-32 Characters except whitespace
+func (pass Password) Valid() bool {
+	validPassword := regexp.MustCompile(`^\S{10,32}$`)
+	return validPassword.MatchString(string(pass))
 }
 
 type UserRole string
