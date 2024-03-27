34ca65898a5179e97fba70a01388753cfb4758c4
Lucas Kirsche <kontakt@lucas-kirsche.de>
2024-03-27 15:08:56 +0100

    add moduleId and DeviceId to group

--- pkg/types/group.go
+++ pkg/types/group.go
@@ -7,8 +7,10 @@ type Group struct {
 
 type GroupMember struct {
 	Id         int64      `json:"id,omitempty"`
+	ModuleId   ModuleId   `json:"moduleId,omitempty"`
 	ModuleName string     `json:"modulename,omitempty"`
 	ModuleType ModuleType `json:"moduletype,omitempty"`
+	DeviceId   DeviceId   `json:"deviceId,omitempty"`
 	DeviceName string     `json:"devicename,omitempty"`
 	DeviceType DeviceType `json:"devicetype,omitempty"`
 	Group      int64      `json:"group,omitempty"`
